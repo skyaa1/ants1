5eeb23a7ea95574591663c8e1813236325b89f5c
yang jin pyoung <skyaaaa1@naver.com>
2016-04-14 18:10:51 +0900

    삭제 추가

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -16,43 +16,56 @@ public function index(){
 
 
 
-  $this->_head();
-  $this->load->view('footer');
+     $this->_head();
+     $this->load->view('footer');
 
 
 }
 
 public function get($id){
 
-  $this->_head();
-  $data1 = $this->topic_model->gets($id);
+      $this->_head();
 
-  $this->load->helper(array('url','html','korean'));
-  $this->load->view
-  ('topic', array('topics'=>$data1));
-  $this->load->view('footer');
+      $data1 = $this->topic_model->gets($id);
+
+      $this->load->helper(array('url','html','korean'));
+      $this->load->view('topic', array('topics'=>$data1, 'delete_id'=>$id));
+      $this->load->view('footer');
 
 }
 
 public function add(){
 
 
-  $this->_head();
+      $this->_head();
 
-  $this->load->library('form_validation');
-  $this->form_validation->set_rules('name', '제목', 'required');
-  $this->form_validation->set_rules('description', '내용', 'required');
-  if ($this->form_validation->run() == FALSE){
-     $this->load->view('add');
+      $this->load->library('form_validation');
+      $this->form_validation->set_rules('name', '제목', 'required');
+      $this->form_validation->set_rules('description', '내용', 'required');
+   if ($this->form_validation->run() == FALSE){
+      $this->load->view('add');
 
-  } else{
-    echo "성공";
+ } else{
+      $name = $this->input->post('name');
+      $description = $this->input->post('description');
+      $insert_id  = $this->topic_model->write($name, $description);
+      $this->load->helper('url');
+      redirect('/topic/get/'.$insert_id);
   }
-
-
-
-  $this->load->view('footer');
+       $this->load->view('footer');
 }
+
+function delete(){
+   $this->_head();
+   $delete = $this->input->post('delete');
+   $delete_data = $this->topic_model->delete($delete);
+
+   $this->load->view('footer');
+   $this->load->helper('url');
+   redirect('/topic');
+
+}
+
 
  function _head(){
    $data = $this->topic_model->get();
--- application/views/topic.php
+++ application/views/topic.php
@@ -9,8 +9,23 @@
      <?=$topics->description?>
      </div>
    </article>
-   <div>
-     <a href="/index.php/topic/add" class="btn btn-primary">쓰기</a>
+
+
+   <div class="span12">
+
    </div>
+   <div class="row">
+     <div class="span4">
+     </div>
+     <div class="span1">
+        <a href="/index.php/topic/add" class="btn btn-primary">쓰기</a>
+     </div>
+      <form class="span1" action="/index.php/topic/delete" method="post">
+      <input type="hidden" name="delete" value="<?= $delete_id?>">
+        <input type="submit" value="삭제" class="btn btn-primary">
+      </form>
+    </div>
+
+
 
 </div>
